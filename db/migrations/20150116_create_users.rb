929c0a5a0961e185c203b58c0d7290ffd9e2b36d
Wes Rich <elopdat@gmail.com>
2015-01-18 20:29:10 -0500

    Changes to DB structure before implementing...

--- db/migrations/20150116_create_users.rb
+++ db/migrations/20150116_create_users.rb
@@ -1,14 +1,19 @@
 Sequel.migration do
   change do
+    run 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+
     create_table :users do
-      column :id, "uuid", default: Sequel::LiteralString.new("uuid_generate_v4()"), null: false
-      column :uid, "text"
-      column :auth, "json"
-      column :games, "json"
-      column :created_at, "timestamp without time zone"
-      column :updated_at, "timestamp without time zone"
+      column :id, :uuid, default: Sequel.function(:uuid_generate_v4), null: false
+      column :uid, :text, unique: true
+      column :name, :text
+      column :avatar, :text
+      column :is_public, :boolean, default: true
+      column :games, :json
+      column :created_at, :timestamp
+      column :updated_at, :timestamp
 
       primary_key [:id]
+      index :uid
     end
   end
 end
